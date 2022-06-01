c455daf3ebedefb81e57d8fcb3a161baea2c780e
MDHA0100 <92270230+MDHA0100@users.noreply.github.com>
2022-06-01 09:35:05 +0300

    one

---
+++ student.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class Student
+{
+	int ID;
+	string fName;
+	string lName;
+	int age;
+	string gender;
+	string uniName;
+	string major;
+	int stage;
+	string system;
+
+};
+
+// this library will contain all the function used in the student section
+
---
+++ teacher.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+// this libaray contains all the functions used in the teacher section
